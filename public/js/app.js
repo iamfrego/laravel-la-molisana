8345363d6fb0b4dd8e505be4a98ae252f93fb25f
iamfrego <mate_freg@tiscali.it>
2021-12-17 13:03:51 +0100

    aaa

--- public/js/app.js
+++ public/js/app.js
@@ -19432,8 +19432,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\matte\OneDrive\Desktop\Boolean\LARAVEL\molisana\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\matte\OneDrive\Desktop\Boolean\LARAVEL\molisana\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Users\matte\OneDrive\Desktop\Boolean\LARAVEL\laravel-la-molisana\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\matte\OneDrive\Desktop\Boolean\LARAVEL\laravel-la-molisana\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
